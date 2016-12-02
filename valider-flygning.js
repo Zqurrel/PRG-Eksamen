9bd692e740b4fa7953f4284a4c22f37384e165e0
BS92 <belal@live.no>
2016-12-02 17:01:17 +0100

    Update valider-flygning.js

--- valider-flygning.js
+++ valider-flygning.js
@@ -1,38 +1,28 @@
 function validerFlygning()
 {
-	var flightnr=document.getElementById("flightnr").value; 
-	var fraflyplass=document.getElementById("fraflyplass").value;
-    var tilflyplass=document.getElementById("tilflyplass").value; 
-	var dato=document.getElementById("dato").value;
-	
-	var feilmelding="";
-	
-	if(!flightnr)
+    var flightnr = document.getElementById('flightnr').value;
+    if (flightnr == "") 
 	{
-		feilmelding="Flightnr er ikke korrekt utfyllt </br>";
-	}
-	if(!fraflyplass)
+        alert("Flightnr er ikke fyllt ut");
+        return false;
+    }
+	var fraflyplass = document.getElementById('fraflyplass').value;
+    if (fraflyplass == "") 
 	{
-		feilmelding=feilmelding +"Fra flyplass er ikke korrekt utfylt </br>";
-	}
-	
-    if(!tilflyplass)
+        alert("Fraflyplass er ikke fyllt ut");
+        return false;
+    }
+	var tilflyplass = document.getElementById('tilflyplass').value;
+    if (tilflyplass == "") 
 	{
-		feilmelding="Til flyplass er ikke korrekt utfyllt </br>";
-	}
-    if(!dato)
+        alert("Tilflyplass er ikke fyllt ut");
+        return false;
+    }
+	var dato = document.getElementById('dato').value;
+    if (dato == "") 
 	{
-		feilmelding="Dato er ikke korrekt utfyllt </br>";
-	}
-
-	if(flightnr && tilflyplass && tilflyplass && dato)
-	{
-		return true;
-	}
-	else
-	{
-		document.getElementById("melding").style.color="red";
-		document.getElementById("melding").innerHTML=feilmelding;
-		return false;
-	}
+        alert("Dato er ikke fyllt ut");
+        return false;
+    }
+  return true;
 }
